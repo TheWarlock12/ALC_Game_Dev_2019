32cfcb6d542d5036da8fa85216377b1cc506e2ad
Unknown <micah.eastwood@gmail.com>
2018-09-07 10:32:45 -0600

    White box texture

--- 2D Game/Assets/Scripts/CharacterMove.cs
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour { 
 
-	public int moveSpeed = 10;
+	// Player movement variables
+	public int moveSpeed;
+	public float JumpHeight;
 
-
+	//Player grounded variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded; 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 	// Update is called once per frame
 	void Update () {
-		
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+			Jump();
+		}
 	}
+	
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+	}
+	
 }
