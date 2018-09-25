e55720def421d5eaed93ddbf88976493fe015696
Unknown <micah.eastwood@gmail.com>
2018-09-25 10:29:08 -0600

    Made Coin, score increases, started Health script

---
+++ 2D Game/Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour {
+
+	public int PointsToAdd;
+
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if (other.GetComponent<Rigidbody2D> () == null)
+			return;
+
+		ScoreManager.AddPoints (PointsToAdd);
+
+		Destroy (gameObject);
+	}
+}
---
+++ 2D Game/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter2D(collider2D) of deathbox	
+	
+}
