ba9a2e8bd4569c13bd66297eaecf4326adbac848
Unknown <micah.eastwood@gmail.com>
2018-11-27 11:09:45 -0700

    Teleport Script

---
+++ 2D Game/Assets/Scripts/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	// Destination variables
+	public Transform Destination;
+	public GameObject PC;
+	// Use this for initialization
+	void Start () {
+		PC = GameObject.Find("PC");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "PC")
+		PC.transform.position = Destination.transform.position;
+	}
+}
